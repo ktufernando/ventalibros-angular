c1b534e0d611d0ffa501b8e4141efbbde2c29e87
Fernando Valdes <fernandoluis.valdes@bbva.com>
2019-05-14 17:17:39 -0300

    fix deploy

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/apps/conduit'));
+app.use(express.static(path.resolve(__dirname, 'dist/apps/conduit')));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/apps/conduit/index.html'));
+    res.sendFile(path.resolve(__dirname,'dist/apps/conduit/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
